125b23ae688f3d1039947331690fbab4d73bec43
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2024-04-01 15:16:09 -0300

    terminal operations

--- session-05-advanced-java/src/main/java/io/javabrains/functionaljava/IntermediateOperations.java
+++ session-05-advanced-java/src/main/java/io/javabrains/functionaljava/IntermediateOperations.java
@@ -8,10 +8,12 @@ public class IntermediateOperations {
     Stream<Integer> limitedStream = numbers.limit(5);
     limitedStream.forEach(System.out::println);
 
+
     numbers = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     Stream<Integer> evenStream = numbers.filter(num -> num % 2 == 0);
     Stream<Integer> firt2Evens = evenStream.limit(2);
     firt2Evens.forEach(System.out::println);
+
 
     numbers = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
     numbers
@@ -19,12 +21,18 @@ public class IntermediateOperations {
       .filter(num -> num % 2 == 0)
       .forEach(System.out::println);
 
-    Stream<String> names = Stream.of("apple", "banana", "mango", "strawberry");
 
-    names
+    Stream<String> names = Stream.of("apple", "banana", "mango", "strawberry");
+    Stream<String> stringStream = names
       .map(String::toUpperCase)
       .peek(str -> System.out.println("Peek result: " + str))
-      .filter(name -> name.length() < 6)
-      .forEach(System.out::println);
+      .filter(name -> name.length() < 6);
+    
+    System.out.println("Nothing of.peek printed so far");
+
+    stringStream.toList(); // need to call a terminal operation so the stream starts!
+    // this happens due to the lazyness of the streams
+
+    System.out.println(".peek printed!");
   }
 }
