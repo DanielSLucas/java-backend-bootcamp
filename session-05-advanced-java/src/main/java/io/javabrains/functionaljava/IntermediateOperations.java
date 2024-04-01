255324eb2260d249f2862c7023612758c41bbd1f
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2024-04-01 15:26:32 -0300

    concatenating streams

--- session-05-advanced-java/src/main/java/io/javabrains/functionaljava/IntermediateOperations.java
+++ session-05-advanced-java/src/main/java/io/javabrains/functionaljava/IntermediateOperations.java
@@ -41,7 +41,11 @@ public class IntermediateOperations {
       .peek(System.out::println)
       .sorted() // sort the elements, but only when the stream is ended, 
       // it needs to consume the entire stream to be able to sort it
-      .forEach(System.out::println);;
+      .forEach(System.out::println);
 
+
+    Stream<Integer> oddNumbers = Stream.of(1, 3, 5, 7, 9);
+    Stream<Integer> evenNumbers = Stream.of(0, 2, 4, 6, 8);
+    Stream.concat(evenNumbers, oddNumbers).forEach(System.out::println);
   }
 }
