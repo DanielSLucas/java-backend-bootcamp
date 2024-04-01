40c5128d8db50fc2b691da89a459415d61314e5b
Daniel Lucas Santos <daniellucas-pms@hotmail.com>
2024-04-01 15:08:31 -0300

    .peek() operator

--- session-05-advanced-java/src/main/java/io/javabrains/functionaljava/IntermediateOperations.java
+++ session-05-advanced-java/src/main/java/io/javabrains/functionaljava/IntermediateOperations.java
@@ -22,7 +22,9 @@ public class IntermediateOperations {
     Stream<String> names = Stream.of("apple", "banana", "mango", "strawberry");
 
     names
-      .map(String::toUpperCase)      
+      .map(String::toUpperCase)
+      .peek(str -> System.out.println("Peek result: " + str))
+      .filter(name -> name.length() < 6)
       .forEach(System.out::println);
   }
 }
